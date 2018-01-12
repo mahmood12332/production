79f4350679e2b91d25d14e392ead2449d10e89a7
Mahmood hassan <mahmoodh12332@gmail.com>
2018-01-12 17:35:01 +0500

    update

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@
 
     // create the module and name it scotchApp
         // also include ngRoute for all our routing needs    
-    var mapp = angular.module("MyApp", ['firebase','ngRoute','ngMap','angular.ujs']);
+    var mapp = angular.module("MyApp", ['firebase','ngRoute','ngMap','angular.ujs','ngCookies']);
          var config = {
             apiKey: "AIzaSyBfy_TN79UUuzWTniXfUN_xFyMAfCZLJeg",
             authDomain: "namazi-5a144.firebaseapp.com",
@@ -81,41 +81,62 @@
               redirectTo: '/' 
             });
     });
-      mapp.controller('MyController', function(NgMap) {
-         var vm = this;
-        NgMap.getMap().then(function(map) {
-          vm.showCustomMarker= function(evt) {
-            map.customMarkers.foo.setVisible(true);
-            map.customMarkers.foo.setPosition(this.getPosition());
-          };
-          vm.closeCustomMarker= function(evt) {
-            this.style.display = 'none';
-          };
-        });
+    mapp.run(function($window) {
+      $window.var1 = 'production';
+      $window.var2 = 'development';
+    });
+    mapp.controller('MyController', function(NgMap) {
+        var vm = this;
+      NgMap.getMap().then(function(map) {
+        vm.showCustomMarker= function(evt) {
+          map.customMarkers.foo.setVisible(true);
+          map.customMarkers.foo.setPosition(this.getPosition());
+        };
+        vm.closeCustomMarker= function(evt) {
+          this.style.display = 'none';
+        };
       });
-      mapp.config(['$qProvider', function ($qProvider) {
-          $qProvider.errorOnUnhandledRejections(false);
-      }]);
+    });
+    mapp.config(['$qProvider', function ($qProvider) {
+        $qProvider.errorOnUnhandledRejections(false);
+    }]);
     // create the controller and inject Angular's $scope
-    mapp.controller('loginController',['$scope', '$firebaseObject', '$firebaseAuth', '$firebaseArray', function($scope, $firebaseObject, $firebaseAuth, $firebaseArray){
-           
-            $scope.fbAuth = $firebaseAuth();
-            $scope.err = {};
-            $scope.btn_hide = true;
-              //var authData = fbAuth.$getAuth();
-            $scope.fbAuth.$onAuthStateChanged(function(firebaseUser) {
-              if (firebaseUser) {
-                console.log("Signed in as:", firebaseUser.email);
-                $scope.btn_show = true;
-                $scope.btn_hide = false;
-              } else {
-                console.log("Signed out");
-                $scope.btn_hide = true;
-                $scope.btn_show = false;
-              }
-            });
-            $scope.username = "namazi@gmail.com";
-            $scope.password= "12345678";
+    mapp.controller('loginController',['$scope', '$firebaseObject', '$firebaseAuth', '$firebaseArray','$window','$cookies', function($scope, $firebaseObject, $firebaseAuth, $firebaseArray,$window,$cookies){              
+      $scope.variables = $window.var1;      
+      $scope.toggleResponseValue = function(response){
+        console.log(response)
+        if(response == true){
+          $scope.variables = $window.var2; //Assume it's not clicked if there's no existing value
+        }else{
+          $scope.variables = $window.var1;
+        }
+      };
+      $scope.SetCookies = function () {
+        $cookies.put("variables",$scope.variables);
+      };
+      $scope.GetCookies = function () {
+          $window.alert($cookies.get('variables'));
+      };
+      $scope.ClearCookies = function () {
+          $cookies.remove('variables');
+      };
+      $scope.fbAuth = $firebaseAuth();
+      $scope.err = {};
+      $scope.btn_hide = true;
+        //var authData = fbAuth.$getAuth();
+      $scope.fbAuth.$onAuthStateChanged(function(firebaseUser) {
+        if (firebaseUser) {
+          console.log("Signed in as:", firebaseUser.email);
+          $scope.btn_show = true;
+          $scope.btn_hide = false;
+        } else {
+          console.log("Signed out");
+          $scope.btn_hide = true;
+          $scope.btn_show = false;
+        }
+      });
+      $scope.username = "namazi@gmail.com";
+      $scope.password= "12345678";
       $scope.logmein = function(username, password) {
         $scope.fbAuth.$signInWithEmailAndPassword(
             username,
@@ -204,11 +225,13 @@
   $scope.populate = function(){
     var authData = $scope.fbAuth.$getAuth();
     if (authData){
-    var refPath = "/development/Users";
+    var refPath = "/development/users";
     so = $firebaseArray(db.ref(refPath));
     so.$add({
       email: $scope.usernames,
-      user_Type: "user"
+      user_Type: "user",
+      fullName:$scope.fullName,
+      phoneNum:$scope.phoneNum
     }).then(function(dat){
       console.log('here-1');
       $scope.err.message = "Data uploaded";
@@ -246,44 +269,53 @@
 
 
 
-   mapp.controller('detailController',['$scope','$firebaseObject', '$firebaseArray', 'Auth','$location','$routeParams', function($scope,$firebaseObject, $firebaseArray, Auth,$location,$routeParams){
-     
-  var aout = Auth.$getAuth();
-  if (aout){
-  // var reff = '/development/Masjids'+aout.Identifier;
-    // if (aout.email=='testadmin@test.com'){
-    //   reff = '/development/Masjids';
-    // }
+  mapp.controller('detailController',['$scope','$firebaseObject', '$firebaseArray', 'Auth','$location','$routeParams','$window', function($scope,$firebaseObject, $firebaseArray, Auth,$location,$routeParams,$window){
+      $scope.variables = $window.var1;      
+      $scope.toggleResponseValue = function(response){
+        console.log(response)
+        if(response == true){
+          $scope.variables = $window.var2; //Assume it's not clicked if there's no existing value
+        }else{
+          $scope.variables = $window.var1;
+        }
+      };
+      var aout = Auth.$getAuth();
+      if (aout){
+      // var reff = '/development/Masjids'+aout.Identifier;
+        // if (aout.email=='testadmin@test.com'){
+        //   reff = '/development/Masjids';
+        // }
 
-    console.log($routeParams.id)
-    // $scope.uid = {};
-    // $scope.lst = {};
-    // $scope.list = [];
-    // $scope.lst = $firebaseArray(db.ref(reff));
-    $scope.newlst =$firebaseObject(db.ref('/development/Masjids').child($routeParams.id));
-    // $scope.user = $firebaseArray(db.ref('/development/Masjids'));
-         
-    // $scope.lst.$loaded().then(function(object){
-    //  $scope.lst = object;
-    //  console.log($scope.lst)
-    // });
+        console.log($routeParams.id)
+        // $scope.uid = {};
+        // $scope.lst = {};
+        // $scope.list = [];
+        // $scope.lst = $firebaseArray(db.ref(reff));
+        $scope.newlst =$firebaseObject(db.ref('/development/Masjids').child($routeParams.id));
+        // $scope.user = $firebaseArray(db.ref('/development/Masjids'));
+            
+        // $scope.lst.$loaded().then(function(object){
+        //  $scope.lst = object;
+        //  console.log($scope.lst)
+        // });
 
-    $scope.newlst.$loaded().then(function(object){
+        $scope.newlst.$loaded().then(function(object){
 
-     $scope.list = object;
-     if (object.length < 1 ) {
-       console.log("true")    
-     } else {
-      console.log("else")   
-     }
-     console.log($scope.list)
-     console.log($scope.list.length)
-    });
-}
+        $scope.list = object;
+        if (object.length < 1 ) {
+          console.log("true")    
+        } else {
+          console.log("else")   
+        }
+        console.log($scope.list)
+        console.log($scope.list.length)
+        });
+    }
 }]);
 
- mapp.controller('adminController',['$scope','$firebaseObject', '$firebaseArray', 'Auth','$location', function($scope,$firebaseObject, $firebaseArray, Auth,$location){
-     
+ mapp.controller('adminController',['$scope','$firebaseObject', '$firebaseArray', 'Auth','$location','$window','$cookies', function($scope,$firebaseObject, $firebaseArray, Auth,$location,$window,$cookies){
+  console.log($cookies.variables);
+  $window.variables = $scope.fbdata;
   var aout = Auth.$getAuth();
   if (aout){
   var reff = '/development/Masjids'+aout.Identifier;
@@ -331,7 +363,7 @@ mapp.controller('requestController',['$scope','$firebaseObject', '$firebaseArray
      
   var aout = Auth.$getAuth();
   if (aout){
-  // var reff = '/development/Masjids'+aout.Identifier;
+    // var reff = '/development/Masjids'+aout.Identifier;
     // if (aout.email=='testadmin@test.com'){
     //   reff = '/development/Masjids';
     // }
